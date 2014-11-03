2e36637126a783197b1ec10e3bc97d662066d706
ykravtsov <ykravtsov1@gmail.com>
2014-11-02 21:50:43 -0800

    creted methods and wrote javadocs

---
+++ src/blackdoor/cqbe/cli/DART.java
@@ -0,0 +1,28 @@
+package blackdoor.cqbe.cli;
+
+public class DART {
+
+	/**
+ 	* This is the main function that is called whenever the user ineracts with dart via cli
+ 	*
+ 	* @param  args  command line args
+ 	*/
+    public static void main(String[] args) {
+    }
+
+    /**
+ 	* Decides what UILogic method to call
+ 	*
+ 	*/
+	public choose_ui_logic() {
+	}
+
+    /**
+ 	* Used to print a menu of commands, their descriptions and uses. 
+ 	* Can be used injunction with a specific command to get just that command’s description.
+ 	*
+ 	* @param  command  to get just that command’s description
+ 	*/
+	public help(command) {
+	}
+}
