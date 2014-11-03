7b7ef384e56d9552e30676689b3dfbe10185a22b
Cyril Van Dyke <cyril.vandyke@gmail.com>
2014-11-02 17:39:28 -0800

    Initial Node API
    Boop

--- src/blackdoor/cqbe/node/Updater.java
+++ src/blackdoor/cqbe/node/Updater.java
@@ -7,4 +7,26 @@ public class Updater {
 		
 	}
 
+	/**
+	 * Checks to see if any updates are needed
+	 * @return Yes if update needed, no otherwise
+	 */
+	public Boolean checkForUpdates(){
+		return null;
+	}
+	
+	/**
+	 * Checks for needed updates then calls appropriate helper functions to update the node
+	 */
+	public void update(){
+		
+	}
+	
+	/**
+	 * Updates the address table of the node
+	 */
+	public void updateAddressTable(){
+		
+	}
+	
 }
