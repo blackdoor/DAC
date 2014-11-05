7742fb9b4630498eed37d6926a33fb409753b2e6
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-11-04 23:49:23 -0800

    Added RPC exceptions and router comments

---
+++ src/blackdoor/cqbe/rpc/RPCException.java
@@ -0,0 +1,51 @@
+package blackdoor.cqbe.rpc;
+
+public class RPCException extends Exception {
+	/**
+	 * Invalid JSON was received by the server.
+	 * An error occurred on the server while parsing the JSON text.
+	 * @author nfischer3
+	 *
+	 */
+	public static class ParseException extends RPCException{
+		
+	}
+	/**
+	 * The JSON sent is not a valid Request object.
+	 * @author nfischer3
+	 *
+	 */
+	public static class InvalidRequestException extends RPCException{
+		
+	}
+	/**
+	 * The method does not exist / is not available.
+	 * @author nfischer3
+	 *
+	 */
+	public static class MethodNotFoundException extends RPCException{
+		
+	}
+	/*
+	 * Invalid method parameter(s).
+	 */
+	public static class InvalidParameterException extends RPCException{
+		
+	}
+	/**
+	 * Internal JSON-RPC error.
+	 * @author nfischer3
+	 *
+	 */
+	public static class InternalException extends RPCException{
+		
+	}
+	/**
+	 * Reserved for implementation-defined server-errors.
+	 * @author nfischer3
+	 *
+	 */
+	public static class ServerException extends RPCException{
+		
+	}
+}
