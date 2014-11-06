5d7284fde226b3d73721cbc0627249d6091459d0
Nathanael Fischer <nfischer3@zagmail.gonzaga.edu>
2014-11-05 17:30:27 -0800

    .

--- src/blackdoor/cqbe/cli/Certificate.java
+++ src/blackdoor/cqbe/cli/Certificate.java
@@ -1,6 +1,58 @@
 package blackdoor.cqbe.cli;
 
+import java.util.Arrays;
+
+import blackdoor.util.CommandLineParser;
+import blackdoor.util.CommandLineParser.Argument;
+import blackdoor.util.CommandLineParser.DuplicateOptionException;
+
 public class Certificate {
+	
+	public static void main(String[] args) {
+		CommandLineParser parser = getParser();
+		System.out.println(parser.getHelpText());
+		
+		if(args.length > 0){
+			switch(args[0]){
+				case "create":	create(Arrays.copyOfRange(args, 1, args.length-1));	
+								break;
+				case "endorse":	endorse(Arrays.copyOfRange(args, 1, args.length-1));	
+								break;
+				case "check":	check(Arrays.copyOfRange(args, 1, args.length-1));	
+								break;
+				case "sign":	sign(Arrays.copyOfRange(args, 1, args.length-1));	
+								break;
+			}
+		}
+	}
+	
+	private static CommandLineParser getParser(){
+		CommandLineParser parser = new CommandLineParser();
+		parser.setExecutableName("cqbe certificate");
+		parser.setUsageHint("mandatory options to long options are mandatory for short options too.\n"
+				+ "");
+		Argument subcommand = new Argument().setLongOption("<subcommand>")
+				.setParam(true).setMultipleAllowed(false).setRequiredArg(true)
+				.setTakesValue(false)
+				.setHelpText("the subcommand of certificate to execute.")
+				.addValue("create");
+		Argument helpArgument = new Argument().setLongOption("help")
+				.setOption("h").setMultipleAllowed(false).setTakesValue(false)
+				.setHelpText("print this help.");
+		try {
+			parser.addArgument(subcommand);
+			parser.addArgument(helpArgument);
+		} catch (DuplicateOptionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return parser;
+	}
+
+	private static void check(String[] copyOfRange) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	public Certificate(String[] args) {
 	}
@@ -10,7 +62,7 @@ public class Certificate {
  	*
  	* @param  args list of arguments
  	*/
-	public void create(String[] args) {
+	public static void create(String[] args) {
 	}
 
 	/**
@@ -18,7 +70,7 @@ public class Certificate {
  	*
  	* @param  args list of arguments
  	*/
-	public void endorse(String[] args) {
+	public static void endorse(String[] args) {
 	}
 
 	/**
@@ -26,7 +78,7 @@ public class Certificate {
  	*
  	* @param  args list of arguments
  	*/
-	public void sign(String[] args) {
+	public static void sign(String[] args) {
 	}
 
 	/**
