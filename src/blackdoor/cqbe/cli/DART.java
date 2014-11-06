02d43b9a778552da5bbee1b73f292d3d986947dc
Cj Buresch <cjburesch@gmail.com>
2014-11-06 11:45:28 -0800

    checkout --

--- src/blackdoor/cqbe/cli/DART.java
+++ src/blackdoor/cqbe/cli/DART.java
@@ -18,146 +18,95 @@ import blackdoor.cqbe.rpc.RPCBuilder;
 
 public class DART {
 
-	/**
-	 * This is the main function that is called whenever the user ineracts with
-	 * dart via cli
-	 *
-	 * @param args
-	 *            command line args
-	 */
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Proper Usage is: java DART args");
-			System.exit(0);
-		}
-		if (args[0].equals("cert")) {
-			String[] args2 = Arrays.copyOfRange(args, 1, args.length);
-			new Certificate(args2);
-		} else if (args[0].equals("keys")) {
-			String[] args2 = Arrays.copyOfRange(args, 1, args.length);
-			Keys.main(args2);
-		} else if (args[0].equals("join")) {
-			String[] args2 = Arrays.copyOfRange(args, 1, args.length);
-			DART.join(args2);
-		}
-		// CommandLineParser clp = new CommandLineParser();
-		// clp.addArgument('h', "Help", "Display help menue", true, false,
-		// false);
-		// clp.addArgument('v', "version", "Displays the version", true,
-		// false, false);
-		// try {
-		// Map out = clp.parseArgs(test1);
-		// if (out.containsKey("help")) {
-		// System.out.println(clp.getHelpText());
-		// } else if (out.containsKey("version")) {
-		// System.out.println("DART 1.0");
-		// }
-		// } catch (Exception e) {
-		// System.out.println(clp.getHelpText());
-		// }
-		else if (args[0].equals("help")) {
-			String[] args2 = Arrays.copyOfRange(args, 1, args.length);
-			DART.help(args2);
-		} else {
-			DART.help(args);
-		}
-	}
+  /**
+   * This is the main function that is called whenever the user ineracts with dart via cli
+   *
+   * @param args command line args
+   */
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Proper Usage is: java DART args");
+      System.exit(0);
+    }
+    if (args[0].equals("cert")) {
+      String[] args2 = Arrays.copyOfRange(args, 1, args.length);
+      new Certificate(args2);
+    } else if (args[0].equals("keys")) {
+      String[] args2 = Arrays.copyOfRange(args, 1, args.length);
+      Keys.main(args2);
+    } else if (args[0].equals("join")) {
+      String[] args2 = Arrays.copyOfRange(args, 1, args.length);
+      DART.join(args2);
+    } else if (args[0].equals("help")) {
+      String[] args2 = Arrays.copyOfRange(args, 1, args.length);
+      DART.help(args2);
+    } else {
+      DART.help(args);
+    }
+  }
 
-	/**
-	 * Used to print a menu of commands, their descriptions and uses. Can be
-	 * used injunction with a specific command to get just that command’s
-	 * description.
-	 *
-	 * @param args
-	 *            list of arguments
-	 */
-	public static void help(String[] args) {
-	}
+  /**
+   * Used to print a menu of commands, their descriptions and uses. Can be used injunction with a
+   * specific command to get just that command’s description.
+   *
+   * @param args list of arguments
+   */
+  public static void help(String[] args) {}
 
-	/**
-	 * join and setup the dart network
-	 *
-	 * @param args
-	 *            list of arguments
-	 */
-	public static void join(String[] args) {
-		// Parse things into
-		Router r = new Router();
-		Address destination = null;
-		int port = -1;
-		Boolean adam = false;
-		String dir = "";
-		String revive = "";
-		Boolean daemon = false;
-		AddressTable neighbors = r.resolveAddress(destination);
+  /**
+   * join and setup the dart network
+   *
+   * @param args list of arguments
+   */
+  public static void join(String[] args) {
+    // Parse things into
+    Router r = new Router();
+    Address destination = null;
+    int port = -1;
+    Boolean adam = false;
+    String dir = "";
+    String revive = "";
+    Boolean daemon = false;
+    AddressTable neighbors = r.resolveAddress(destination);
 
-		NodeBuilder bob = new NodeBuilder(neighbors);
-		bob.setAdam(adam);
-		bob.setDaemon(daemon);
-		if (dir != "")
-			bob.setDirectory(dir);
-		if (revive != "")
-			bob.setRevival(revive);
-		if (port != -1)
-			bob.setPort(port);
+    NodeBuilder bob = new NodeBuilder(neighbors);
+    bob.setAdam(adam);
+    bob.setDaemon(daemon);
+    if (dir != "")
+      bob.setDirectory(dir);
+    if (revive != "")
+      bob.setRevival(revive);
+    if (port != -1)
+      bob.setPort(port);
 
-		bob.buildNode();
-	}
+    bob.buildNode();
+  }
 
-	/**
-	 * Insert certfile(s) into the network
-	 *
-	 * @param args
-	 *            list of arguments
-	 */
-	public static void insert(String[] args) {
-		// CommandLineParser clp = new CommandLineParser();
-		//
-		// try {
-		// clp.addArgument('h', "Help", "Display help menue", true, false,
-		// false);
-		// clp.addArgument('b', "bootstrap", "the bootstrap node Address:Port",
-		// true, false, false);
-		// Map out = clp.parseArgs(test1);
-		// if (out.containsKey("help")) {
-		// System.out.println(clp.getHelpText());
-		// } else if (out.containsKey("bootstrap")) {
-		// Address a = new Address(args[2]);
-		// RPCBuilder rpcObject = new RPCBuilder();
-		// JSONObject rpc = rpcObject.buildPUT(args[0]);
-		// Router router = new Router(a);
-		// router.routeWithCalls(rpc);
-		// } else {
-		// RPCBuilder rpcObject = new RPCBuilder();
-		// JSONObject rpc = rpcObject.buildPUT(args[0]);
-		// Router router = new Router();
-		// router.routeWithCalls(rpc);
-		// }
-		// } catch (Exception e) {
-		// System.out.println(clp.getHelpText());
-		// }
+  /**
+   * Insert certfile(s) into the network
+   *
+   * @param args list of arguments
+   */
+  public static void insert(String[] args) {
 
-	}
+  }
 
-	/**
-	 * Finds a specific certificate in the network
-	 *
-	 * @param args
-	 *            list of arguments
-	 */
-	public static void retrieve(String[] args) {
-	}
+  /**
+   * Finds a specific certificate in the network
+   *
+   * @param args list of arguments
+   */
+  public static void retrieve(String[] args) {}
 
-	/**
-	 * Used to exit the network
-	 *
-	 * @param args
-	 *            list of arguments
-	 */
-	public static void shutdown(String[] args) {
-		// CrazyIvan
-		// RPCBuilder r = new RPCBuilder();
-		// r.buildSHUTDOWN();
+  /**
+   * Used to exit the network
+   *
+   * @param args list of arguments
+   */
+  public static void shutdown(String[] args) {
+    // CrazyIvan
+    // RPCBuilder r = new RPCBuilder();
+    // r.buildSHUTDOWN();
 
-	}
+  }
 }
--- src/blackdoor/cqbe/cli/Keys.java
+++ src/blackdoor/cqbe/cli/Keys.java
@@ -4,8 +4,15 @@
 package blackdoor.cqbe.cli;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
 import java.io.Console;
+
+import blackdoor.util.CommandLineParser;
+import blackdoor.util.DBP;
+import blackdoor.util.CommandLineParser.Argument;
+import blackdoor.util.CommandLineParser.DuplicateOptionException;
+import blackdoor.util.CommandLineParser.InvalidFormatException;
 
 /**
  * 
@@ -14,84 +21,128 @@ import java.io.Console;
  */
 public class Keys {
 
-	public static void main(String[] args) {
-		Keys kl = new Keys();
-		if (args[0].equals("create")) {
-			kl.create(Arrays.copyOfRange(args, 1, args.length));
-		} else if (args[0].equals("retrieve")) {
-			kl.retrive(Arrays.copyOfRange(args, 1, args.length));
-		} else if (args[0].equals("remove")) {
-			kl.remove(Arrays.copyOfRange(args, 1, args.length));
-		} else {
-			// Something went wrong. Handle this.
-		}
-	}
+  public static void main(String[] args) {
+    DBP.DEBUG = true;
+    DBP.ERROR = true;
 
-	/**
-	 * Creates a keypair and stores it encrypted in a keystore.
-	 * <p>
-	 *
-	 * @param args
-	 *            List of Arguments.
-	 */
-	public void create(String[] args) {
-		String password = scannerPrompt(); // for IDE TESTING ONLY
-	}
+    CommandLineParser parser = getParser();
+    Keys kl = new Keys();
+    Map<String, Argument> parsedArgs;
+    try {
+      parsedArgs = parser.parseArgs(args);
+      if (parsedArgs.containsKey("subcommand")) {
+        switch (args[0]) {
+          case "create":
+            create(Arrays.copyOfRange(args, 1, args.length));
+            break;
+          case "retrieve":
+            Keys.retrive(Arrays.copyOfRange(args, 1, args.length));
+            break;
+          case "remove":
+            Keys.remove(Arrays.copyOfRange(args, 1, args.length));
+            break;
+        }
+      } else {
+        System.out.println(parser.getHelpText());
 
-	/**
-	 * Retrieves a key from a keystore.
-	 * <p>
-	 *
-	 * @param args
-	 *            List of Arguments.
-	 */
-	public void retrive(String[] args) {
-		String password = scannerPrompt(); // for IDE TESTING ONLY
-	}
+      }
+    } catch (InvalidFormatException e) {
+      System.out.println(parser.getHelpText());
+      DBP.printerrorln(e.getMessage());
+      for (StackTraceElement elem : e.getStackTrace()) {
+        DBP.printerrorln(elem);
+      }
+    }
+  }
 
-	/**
-	 * Removes a key from a keystore.
-	 * <p>
-	 *
-	 * @param args
-	 *            List of Arguments.
-	 */
-	public void remove(String[] args) {
-		String password = scannerPrompt(); // for IDE TESTING ONLY
-	}
+  private static CommandLineParser getParser() {
+    CommandLineParser parser = new CommandLineParser();
+    parser.setExecutableName("cqbe keys");
+    parser
+        .setUsageHint("\tmandatory options to long options are mandatory for short options too.\n"
+            + "The subcommands for cqbe keys are:\n" + "\t create \n" + "\t retrieve \n"
+            + "\t remove \n");
+    Argument subcommand =
+        new Argument().setLongOption("subcommand").setParam(true).setMultipleAllowed(false)
+            .setRequiredArg(false).setTakesValue(false)
+            .setHelpText("the subcommand of certificate to execute.");
+    // DBP.printdebugln(subcommand);
+    Argument helpArgument =
+        new Argument().setLongOption("help").setOption("h").setMultipleAllowed(false)
+            .setTakesValue(false).setHelpText("print this help.");
+    try {
+      parser.addArgument(subcommand);
+      parser.addArgument(helpArgument);
+    } catch (DuplicateOptionException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return parser;
+  }
 
-	/**
-	 * Gets password from user input.
-	 * <p>
-	 * Characters are hidden from console, and console history.
-	 * 
-	 * @return password input.
-	 */
-	private String consolePrompt() {
-		Console cons = null;
-		char[] passwd = null;
-		if ((cons = System.console()) != null && (passwd = cons.readPassword("[%s]", "enter password:")) != null) {
-			java.util.Arrays.fill(passwd, ' ');
-		} else {
-			// Handle a null console, or no password entry!
-		}
-		return passwd.toString();
-	}
+  /**
+   * Creates a keypair and stores it encrypted in a keystore.
+   * <p>
+   *
+   * @param args List of Arguments.
+   */
+  public static void create(String[] args) {
+    String password = scannerPrompt(); // for IDE TESTING ONLY
+  }
 
-	/**
-	 * Gets password from user input.
-	 * <p>
-	 * USED ONLY FOR IDE TESTING. Will expose password to console history, which
-	 * is a security flaw. Do not do that.
-	 * 
-	 * @return password input.
-	 */
-	private String scannerPrompt() {
-		System.out.println("enter password:");
-		Scanner s = new Scanner(System.in);
-		String password = s.next();
-		// Check password format
+  /**
+   * Retrieves a key from a keystore.
+   * <p>
+   *
+   * @param args List of Arguments.
+   */
+  public static void retrive(String[] args) {
+    String password = scannerPrompt(); // for IDE TESTING ONLY
+  }
 
-		return password;
-	}
+  /**
+   * Removes a key from a keystore.
+   * <p>
+   *
+   * @param args List of Arguments.
+   */
+  public static void remove(String[] args) {
+    String password = scannerPrompt(); // for IDE TESTING ONLY
+  }
+
+  /**
+   * Gets password from user input.
+   * <p>
+   * Characters are hidden from console, and console history.
+   * 
+   * @return password input.
+   */
+  private static String consolePrompt() {
+    Console cons = null;
+    char[] passwd = null;
+    if ((cons = System.console()) != null
+        && (passwd = cons.readPassword("[%s]", "enter password:")) != null) {
+      java.util.Arrays.fill(passwd, ' ');
+    } else {
+      // Handle a null console, or no password entry!
+    }
+    return passwd.toString();
+  }
+
+  /**
+   * Gets password from user input.
+   * <p>
+   * USED ONLY FOR IDE TESTING. Will expose password to console history, which is a security flaw.
+   * Do not do that.
+   * 
+   * @return password input.
+   */
+  private static String scannerPrompt() {
+    System.out.println("enter password:");
+    Scanner s = new Scanner(System.in);
+    String password = s.next();
+    // Check password format
+
+    return password;
+  }
 }
