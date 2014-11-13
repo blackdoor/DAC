e0ea7b99cbc0ea0b3f03d7b6dce6a146f5a7a703
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-11-13 02:58:56 -0800

    early work on address table
    address is done

--- src/blackdoor/cqbe/addressing/AddressTable.java
+++ src/blackdoor/cqbe/addressing/AddressTable.java
@@ -1,27 +1,55 @@
 package blackdoor.cqbe.addressing;
 
+import java.util.Comparator;
 import java.util.concurrent.ConcurrentSkipListMap;
 
-import blackdoor.cqbe.addressing.Address.OverlayAddress;
+import blackdoor.cqbe.addressing.Address.AddressComparator;
+
 
 /**
  * 
  * @author Cj Buresch
  * @version v0.0.1 - Nov 5, 2014
  */
-public class AddressTable {
+public class AddressTable extends ConcurrentSkipListMap<byte[], Address>{
+	
+	
+	public static final int MAX_SIZE = 256;
+	
+	public AddressTable(Address refrence){
+		super(new Address.OverlayComparator(refrence));
+	}
+	
+	/**
+	 * Associates the specified value with the specified key in this table. 
+	 * If the table previously contained a mapping for the key, the old value is replaced.
+	 * If the table did not previously contain a mapping for key and the table is full, then farthest entry is removed from the table.
+	 * @param key overlay address with which the specified Address is to be associated
+	 * @param value Address to be associated with the specified overlay address
+	 * @return the previous value associated with the specified key, or null if there was no mapping for the key, or the value associated with the farthest entry which was removed as a result of this put
+	 */
+	public Address put(byte[] key, Address value){
+		Address absent = putIfAbsent(key, value);
+		if(size() >= MAX_SIZE && absent == null){
+			return pollLastEntry().getValue();
+		}
+		return absent;
+	}
+	
 
-	private static ConcurrentSkipListMap<OverlayAddress, Address> table = new ConcurrentSkipListMap<OverlayAddress, Address>();;
+	//private ConcurrentSkipListMap<Address, Address> table;// = new ConcurrentSkipListMap<OverlayAddress, Address>();;
 
 	public AddressTable() {
 	}
 
 	/**
 	 * Likely redundant. But who knows.
-	 */
+	 *
 	public static void init() {
 
 	}
+	yea, redundant
+	*/
 
 	/**
 	 * Adds an address to the address table.
@@ -55,15 +83,6 @@ public class AddressTable {
 	 * @return True or false depending on if the address table contains
 	 */
 	public static void containsAddress() {
-	}
-
-	/**
-	 * Returns the number of addresses in the table.
-	 * <p>
-	 * 
-	 * @return Integer between 0 and 256.
-	 */
-	public static void size() {
 	}
 
 	/**
