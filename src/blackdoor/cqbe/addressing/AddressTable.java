3d7c07dc24b3147116348ed300457f5d9e6134fb
Nathanael Fischer <nfischer3@zagmail.gonzaga.edu>
2014-11-13 17:33:29 -0800

    .

--- src/blackdoor/cqbe/addressing/AddressTable.java
+++ src/blackdoor/cqbe/addressing/AddressTable.java
@@ -1,9 +1,14 @@
 package blackdoor.cqbe.addressing;
 
 import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 import blackdoor.cqbe.addressing.Address.AddressComparator;
+import blackdoor.cqbe.addressing.AddressException.MissingLayer3Exception;
+import blackdoor.util.DBP;
 
 
 /**
@@ -21,22 +26,65 @@ public class AddressTable extends ConcurrentSkipListMap<byte[], Address>{
 	}
 	
 	/**
-	 * Associates the specified value with the specified key in this table. 
-	 * If the table previously contained a mapping for the key, the old value is replaced.
-	 * If the table did not previously contain a mapping for key and the table is full, then farthest entry is removed from the table.
-	 * @param key overlay address with which the specified Address is to be associated
+	 * Adds the specified value to this table, they key is implied by the overlay address of the value. 
+	 * If the table previously contained this address, the old address is replaced.
+	 * If the table did not previously contain the address and the table is full, then farthest entry is removed from the table.
 	 * @param value Address to be associated with the specified overlay address
 	 * @return the previous value associated with the specified key, or null if there was no mapping for the key, or the value associated with the farthest entry which was removed as a result of this put
+	 * @throws MissingLayer3Exception thrown if value does not have a layer 3 address defined.
 	 */
-	public Address put(byte[] key, Address value){
-		Address absent = putIfAbsent(key, value);
-		if(size() >= MAX_SIZE && absent == null){
-			return pollLastEntry().getValue();
+	public Address add(Address value) throws MissingLayer3Exception{
+		if(!value.hasLayer3())
+			throw new AddressException.MissingLayer3Exception();
+		Address absent = putIfAbsent(value.getOverlayAddress(), value);
+		if(absent == null){
+			absent = put(null, value);
+			if(size() >= MAX_SIZE){
+				return pollLastEntry().getValue();
+			}
 		}
 		return absent;
 	}
 	
-
+	public void addAll(Set<Address> c) throws MissingLayer3Exception{
+		for(Address a : c){
+			add(a);
+		}
+	}
+	
+	@Deprecated
+	public Address put(byte[] key, Address value){
+		return super.put(value.getOverlayAddress(), value);
+	}
+	
+	public boolean containsValue(Address value){
+		return containsKey(value.getOverlayAddress());
+	}
+	
+	/**
+	 * Obtains an Address table containing the k nearest addresses to reference
+	 * @param k the number of nearby addresses to retrieve; less than k addresses may be returned, but not more
+	 * @param reference 
+	 * @return a new AddressTable (sorted with respect to reference) containing the k nearest addresses to reference
+	 */
+	public AddressTable getNearestAddresses(int k, Address reference){
+		int i;
+		AddressTable ret = new AddressTable(reference);
+		NavigableSet<Address> sortedAddresses = new ConcurrentSkipListSet<Address>(new AddressComparator(reference));
+		sortedAddresses.addAll(this.values());
+		try {
+			i = 0;
+			for(Address a : sortedAddresses){
+				ret.add(a);
+				if(i++ > k)
+					break;
+			}
+		} catch (MissingLayer3Exception e) {
+			DBP.printException(e);
+		}
+		return ret;
+	}
+	
 	//private ConcurrentSkipListMap<Address, Address> table;// = new ConcurrentSkipListMap<OverlayAddress, Address>();;
 
 	public AddressTable() {
