e70f8e48d59a4124c09f7c11b6c6ef02e46a3263
Cj Buresch <cjburesch@gmail.com>
2015-02-10 13:37:16 -0800

    stub

---
+++ src/blackdoor/cqbe/output_logic/RouterUnitTest.java
@@ -0,0 +1,39 @@
+package blackdoor.cqbe.output_logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import blackdoor.cqbe.node.Node;
+
+public class RouterUnitTest {
+	
+	private Node node;
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
