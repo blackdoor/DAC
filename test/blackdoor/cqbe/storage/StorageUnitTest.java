6035e057bc7ecd0acc0da23ae95a7adef175fc82
Cyril Van Dyke <cyril.vandyke@gmail.com>
2015-02-18 00:16:19 -0800

    .
    .

---
+++ test/blackdoor/cqbe/storage/StorageUnitTest.java
@@ -0,0 +1,19 @@
+package blackdoor.cqbe.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class StorageUnitTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
