bf3e29eecead05badd9c69ac7ba40847b60b6ee8
akocer <annakocer@gmail.com>
2015-03-16 09:59:14 -0700

    package-info.java file for each package

--- src/blackdoor/cqbe/addressing/package-info.java
+++ src/blackdoor/cqbe/addressing/package-info.java
@@ -3,6 +3,12 @@
  */
 /**
  * @author Nathan Fischer and Anna Kocer
- *
+ * This package contains the necessary classes for all possible addresses that
+ * could be encountered in the overlay network, as well as an Address Table
+ * containing their mappings. Each node is associated with an overlay address, 
+ * which is produced via a crypotgraphically secure hash function. 
+ * L3Address is a subclass of Address derived from a network layer address
+ * and port. AddressTable consists of the mappings from overlay addresses to 
+ * layer 3 addresses.
  */
 package blackdoor.cqbe.addressing;
--- src/blackdoor/cqbe/keystore/package-info.java
+++ src/blackdoor/cqbe/keystore/package-info.java
@@ -2,7 +2,9 @@
  * 
  */
 /**
- * @author nfischer3
- *
+ * @author
+ * Contains a KeyStore class which handles the encryption and retrieval of keys correlated
+ * with the storage location for the user's private keys, as well as the input and output
+ * of files.
  */
 package blackdoor.cqbe.keystore;
